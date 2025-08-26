ef7a8926452d0e66bf5bca4112e27ebd784b04b1
sunqi <zqbysq@163.com>
2025-08-26 18:34:53 +0800

    update reactive

--- docs/《vue_core_v3.5.18源码学习》/01.reactivity响应式/tmp.js
+++ docs/《vue_core_v3.5.18源码学习》/01.reactivity响应式/tmp.js
@@ -1,78 +1,36 @@
-const targetMap = new WeakMap();
-
-function track(target, type, key) {
-    if (shouldTrack && activeSub) {
-        let depsMap = targetMap.get(target);
-        if (!depsMap) {
-            targetMap.set(target, depsMap = new Map());
-        }
-        let dep = depsMap.get(key);
-        if (!dep) {
-            depsMap.set(key, dep = new Dep());
-            dep.map = depsMap;
-            dep.key = key;
-        }
-        dep.track();
-    }
-}
-
-function trigger(target, type, key, newValue, oldValue, oldTarget) {
-    const depsMap = targetMap.get(target);
-    if (!depsMap) {
-        globalVersion++;
+function refreshComputed(computed) {
+    if (computed.flags & 4 && !(computed.flags & 16)) {
         return;
     }
-    const run = (dep) => {
-        if (dep) {
-            dep.trigger();
-        }
-    };
-    startBatch();
-    if (type === "clear") {
-        depsMap.forEach(run);
-    } else {
-        const targetIsArray = isArray(target);
-        const isArrayIndex = targetIsArray && isIntegerKey(key);
-        if (targetIsArray && key === "length") {
-            const newLength = Number(newValue);
-            depsMap.forEach((dep, key2) => {
-                if (key2 === "length" || key2 === ARRAY_ITERATE_KEY || !isSymbol(key2) && key2 >= newLength) {
-                    run(dep);
-                }
-            });
-        } else {
-            if (key !== void 0 || depsMap.has(void 0)) {
-                run(depsMap.get(key));
-            }
-            if (isArrayIndex) {
-                run(depsMap.get(ARRAY_ITERATE_KEY));
-            }
-            switch (type) {
-                case "add":
-                    if (!targetIsArray) {
-                        run(depsMap.get(ITERATE_KEY));
-                        if (isMap(target)) {
-                            run(depsMap.get(MAP_KEY_ITERATE_KEY));
-                        }
-                    } else if (isArrayIndex) {
-                        run(depsMap.get("length"));
-                    }
-                    break;
-                case "delete":
-                    if (!targetIsArray) {
-                        run(depsMap.get(ITERATE_KEY));
-                        if (isMap(target)) {
-                            run(depsMap.get(MAP_KEY_ITERATE_KEY));
-                        }
-                    }
-                    break;
-                case "set":
-                    if (isMap(target)) {
-                        run(depsMap.get(ITERATE_KEY));
-                    }
-                    break;
-            }
-        }
+    computed.flags & =-17;
+    if (computed.globalVersion === globalVersion) {
+        return;
     }
-    endBatch();
+    computed.globalVersion = globalVersion;
+    if (!computed.isSSR && computed.flags & 128 && (!computed.deps && !computed._dirty || !isDirty(computed))) {
+        return;
+    }
+    computed.flags |= 2;
+    const dep = computed.dep;
+    const prevSub = activeSub;
+    const prevShouldTrack = shouldTrack;
+    activeSub = computed;
+    shouldTrack = true;
+    try {
+        prepareDeps(computed);
+        const value = computed.fn(computed._value);
+        if (dep.version === 0 || hasChanged(value, computed._value)) {
+            computed.flags |= 128;
+            computed._value = value;
+            dep.version++;
+        }
+    } catch (err) {
+        dep.version++;
+        throw err;
+    } finally {
+        activeSub = prevSub;
+        shouldTrack = prevShouldTrack;
+        cleanupDep(computed);
+        computed.flags &= -3;
+    }
 }
