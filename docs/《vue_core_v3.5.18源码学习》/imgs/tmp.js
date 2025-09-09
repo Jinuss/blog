8f719928513ed5b5026279b6d3f793f598e2bece
sunqi <zqbysq@163.com>
2025-09-09 18:26:43 +0800

    update blog

--- docs/《vue_core_v3.5.18源码学习》/imgs/tmp.js
+++ docs/《vue_core_v3.5.18源码学习》/imgs/tmp.js
@@ -1,3 +1,16 @@
-function watchSyncEffect(effect, options) {
-    return doWatch(effect, null, { flush: "sync" })
+function instanceWatch(source, value, options) {
+    const publicThis = this.proxy;
+    const getter = shared.isString(source) ? source.includes(".") ? createPathGetter(publicThis, source) : () => publicThis[source] : source.bind(publicThis, publicThis);
+    let cb;
+    if (shared.isFunction(value)) {
+        cb = value;
+    } else {
+        cb = value.handler;
+        options = value;
+    }
+
+    const reset = setCurrentInstance(this);
+    const res = doWatch(getter, cb.bind(publicThis), options);
+    reset();
+    return res;
 }
