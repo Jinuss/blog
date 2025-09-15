0df4691d8241e1c38805125c83b6645570e46438
sunqi <zqbysq@163.com>
2025-09-15 22:24:58 +0800

    update

--- docs/《vue_core_v3.5.18源码学习》/imgs/2.js
+++ docs/《vue_core_v3.5.18源码学习》/imgs/2.js
@@ -1,9 +1,92 @@
 let renderer;
 
-const rendererOptions = /* @__PURE__ */ shared.extend({ patchProp }, nodeOps);
+const nodeOps = {
+    insert: (child, parent, anchor) => {
+        parent.insertBefore(child, anchor || null);
+    },
+    remove: (child) => {
+        const parent = child.parentNode;
+        if (parent) {
+            parent.removeChild(child)
+        }
+    },
+    createElement: (tag, namespace, is, props) => {
+        const el = namespace === 'svg' ? doc.createElementNS(svgNS, tag) : namespace === 'mathtml' ? doc.createElementNS(mathmlNS, tag) : is ? doc.createElement(tag, { is }) : doc.createElement(tag);
+        if (tag === "select" && props && props.multiple != null) {
+            el.setAttribute("multiple", props.multipe);
+        }
+        return el;
+    },
+    createText: (text) => doc.createTextNode(text),
+    createComment: () => doc.createComment(text),
+    setText: (node, text) => {
+        node.nodeValue = text;
+    },
+    setElementText: (el, text) => { el.textContent = text },
+    parentNode: (node) => node.parentNode,
+    nextSibling: (node) => node.nextSibling,
+    querySelector: (selector) => doc.querySelector(selector),
+    setScopedId: (el, id) => {
+        el.setAttribute(id, "")
+    },
+    insertStaticContent(conten, parent, anchor, nameSpace, start, end) {
+        const before = anchor ? anchor.previousSibling : parent.lastChild;
+        if (start && (start === end || start.nextSibling)) {
+            while (true) {
+                parent.insertBefore(start.cloneNode(true), anchor);
+                if (start === end || !(start = start.nextSibling)) {
+                    break;
+                }
+            }
+        } else {
+            templateContainer.innerHTML = unsafeToTrustedhTML(namespace === 'svg' ? `<svg>${content}</svg>` : namespace === "mathml" ? `<math>${content}</math>` : content
+            );
+            const template = templateContainer.content;
+            if (namespace === 'svg' || namespace === 'mathml') {
+                const wrapper = template.firstChild;
+                while (wrapper.firstChild) {
+                    template.appendChild(wrapper.firstChild)
+                }
+                template.removeChild(wrapper)
+            }
+            parent.insertBefore(template, anchor);
+        }
+
+        return [before ? before.nextSibling : parent.firstChild, anchor ? anchor.previousSibling : parent.lastChild]
+    }
+}
+
+const patchProp = (el, key, prevValue, nextValue, namespace, parentComponent) => {
+    const isSVG = namespace === 'svg';
+    if (key === 'class') {
+        patchClass(el, nextValue, isSVG);
+    } else if (key === "style") {
+        patchStyle(key, prevValue, nextValue);
+    } else if (shared.isOn(key)) {
+        if (!shared.isModeListener(key)) {
+            patchEvent(el, key, prevValue, nextValue, parentComponent);
+        }
+    } else if (key[0] === '.' ? (key = key.slice(1), true) : key[0] === "^" ? (key = key.slice(1), false) : shouldSetAsProp(el, key, nextValue, isSVG)) {
+        patchDOMProp(el, key, nextValue);
+        if (!el.tagName.includes("-") && (key === "value" || key === "checked" || key === "selected")) {
+            patchAttr(el, key, nextValue, isSVG, parentComponent, key !== "value")
+        }
+    } else if (el._isVueCE && (/[A-Z]/.test(key) || !shared.isstring(nextValue))) {
+        patchDOMProp(el, shared, camelize(key), nextValue, parentComponent, key)
+    } else {
+        if (key === 'true-value') {
+            el._trueValue = nextValue;
+        } else if (key === 'false-value') {
+            el._falseValue = nextValue;
+        }
+        patchAttr(el, key, nextValue, isSVG)
+    }
+}
+
+const rendererOptions = shared.extend({ patchProp }, nodeOps);
 
 function ensureRenderer() {
-  return renderer || (renderer = runtimeCore.createRenderer(rendererOptions));
+    return renderer || (renderer = runtimeCore.createRenderer(rendererOptions));
 }
 
 const createApp = (...args) => {
