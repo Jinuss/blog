a3d3e961c9c307755d03478d55c9f632e86b8bc3
sunqi <zqbysq@163.com>
2025-09-25 17:41:26 +0800

    update

--- docs/《vue_core_v3.5.18源码学习》/imgs/tmp.js
+++ docs/《vue_core_v3.5.18源码学习》/imgs/tmp.js
@@ -1,76 +1,17 @@
-function createComponentInstance(vnode, parent, suspense) {
-  const type = vnode.type;
-  const appContext = (parent ? parent.appContext : vnode.appContext) || emptyAppContext;
-  const instance = {
-    uid: uid++,
-    vnode,
-    type,
-    parent,
-    appContext,
-    root: null,
-    next: null,
-    subTree: null,
-    effect: null,
-    update: null,
-    job: null,
-    scope: new reactivity.EffectScope(
-      true
-    ),
-    render: null,
-    proxy: null,
-    exposed: null,
-    exposeProxy: null,
-    withProxy: null,
-    provides: parent ? parent.provides : Object.create(appContext.provides),
-    ids: parent ? parent.ids : ["", 0, 0],
-    accessCache: null,
-    renderCache: [],
-    components: null,
-    directives: null,
-    propsOptions: normalizePropsOptions(type, appContext),
-    emitsOptions: normalizeEmitsOptions(type, appContext),
-    emit: null,
-    emitted: null,
-    propsDefaults: shared.EMPTY_OBJ,
-    inheritAttrs: type.inheritAttrs,
-    ctx: shared.EMPTY_OBJ,
-    data: shared.EMPTY_OBJ,
-    props: shared.EMPTY_OBJ,
-    attrs: shared.EMPTY_OBJ,
-    slots: shared.EMPTY_OBJ,
-    refs: shared.EMPTY_OBJ,
-    setupState: shared.EMPTY_OBJ,
-    setupContext: null,
-    // suspense related
-    suspense,
-    suspenseId: suspense ? suspense.pendingId : 0,
-    asyncDep: null,
-    asyncResolved: false,
-    isMounted: false,
-    isUnmounted: false,
-    isDeactivated: false,
-    bc: null,
-    c: null,
-    bm: null,
-    m: null,
-    bu: null,
-    u: null,
-    um: null,
-    bum: null,
-    da: null,
-    a: null,
-    rtg: null,
-    rtc: null,
-    ec: null,
-    sp: null
-  };
-  {
-    instance.ctx = { _: instance };
+const patchUnkeyedChildren = (c1, c2, container, anchor, parentComponent, parentSuspense, namespace, slotScopeIds, optimized) => {
+  c1 = c1 || shared.EMPTY_ARR;
+  c2 = c2 || shared.EMPTY_ARR;
+  const oldLength = c1.length;
+  const newLength = c2.length;
+  const commonLength = Math.min(oldLength, newLength);
+  let i;
+  for (i = 0; i < commonLength; i++) {
+    const nextChild = c2[i] = optimized ? cloneIfMounted(c2[i]) : normalizeVNode(c2[i]);
+    patch(c1[i], nextChild, container, null, parentComponent, parentSuspense, namespace, slotScopeIds, optimized)
   }
-  instance.root = parent ? parent.root : instance;
-  instance.emit = emit.bind(null, instance);
-  if (vnode.ce) {
-    vnode.ce(instance);
+  if (oldLength > newLength) {
+    unmountChildren(c1, parentComponent, parentSuspense, true, false, commonLength);
+  } else {
+    mountChildren(c2, container, anchor, parentComponent, parentSuspense, namespace, slotScopeIds, optimized, commonLength)
   }
-  return instance;
 }
